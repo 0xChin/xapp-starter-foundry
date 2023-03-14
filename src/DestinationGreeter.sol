f1634e9dddbce2395afd042e414b290a41120fc3
AlanRacciatti <alanracciatti1220@gmail.com>
2023-03-13 23:57:52 -0300

    feat: add poc

--- src/DestinationGreeter.sol
+++ src/DestinationGreeter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.15;
+pragma solidity 0.8.19;
 
 import {IXReceiver} from "@connext/interfaces/core/IXReceiver.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
